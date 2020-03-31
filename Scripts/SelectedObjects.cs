8daa6d33cca9fa7c06aa0a10f821565b5a949b8c
Colins <crazypad@qq.com>
2020-03-31 18:44:59 +0800

    Update

--- Scripts/SelectedObjects.cs
+++ Scripts/SelectedObjects.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HighlightingSystem;
+
 
 public class SelectedObjects : MonoBehaviour
 {
@@ -8,6 +10,9 @@ public class SelectedObjects : MonoBehaviour
 	private Vector2 endPos;
 
 	private Texture2D guiTexture;
+
+	public List<GameObject> FreedomObjs;
+	private List<GameObject> selectedObjs;
 
 	private void Start()
 	{
@@ -35,11 +40,61 @@ public class SelectedObjects : MonoBehaviour
 
 		if (Input.GetMouseButtonUp(0))
 		{
+			selectAllObjects();
 			startPos = Vector2.zero;
 			endPos = Vector2.zero;
 		}
 
 	}
+
+	private void selectAllObjects()
+	{
+		Vector2 lbP = Vector2.zero;
+		Vector2 rtP = Vector2.zero;
+
+		if (startPos.x > endPos.x)
+		{
+			lbP.x = endPos.x;
+			rtP.x = startPos.x;
+		}
+		else {
+			lbP.x = startPos.x;
+			rtP.x = endPos.x;
+		}
+
+		if (startPos.y > endPos.y)
+		{
+			lbP.y = endPos.y;
+			rtP.y = startPos.y;
+		}
+		else {
+			rtP.y = endPos.y;
+			lbP.y = startPos.y;
+		}
+
+		// Debug.Log("left bottom: " + lbP + "right top: " + rtP);
+
+		Camera camera = Camera.main;
+		for (int i = 0; i < FreedomObjs.Count; i++)
+		{
+			Vector3 location = camera.WorldToScreenPoint(FreedomObjs[i].transform.position);
+
+			// Debug.Log("物体位置：" + location);
+
+			if (location.x < lbP.x || location.x > rtP.x || location.y < lbP.y || location.y > rtP.y
+				|| location.z < camera.nearClipPlane || location.z > camera.farClipPlane)
+			{
+				// false
+				FreedomObjs[i].GetComponent<Highlighter>().enabled = false;
+			}
+			else {
+				// true
+				FreedomObjs[i].GetComponent<Highlighter>().enabled = true;
+			}
+		}
+
+	}
+
 
 	// GUI Canver 左上角为中心点
 	private void OnGUI()
@@ -51,5 +106,10 @@ public class SelectedObjects : MonoBehaviour
 		GUI.skin.box.border = new RectOffset();
 
 		GUI.Box(boxRect, GUIContent.none);
+	}
+
+	private void OnPostRener() 
+	{ 
+		
 	}
 }
